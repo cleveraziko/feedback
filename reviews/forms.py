5f6cf2433eea7bc934f5c12a421fd218fc80d2b6
cleveraziko <74561277+cleveraziko@users.noreply.github.com>
2021-09-09 19:02:43 +0500

    ....

---
+++ reviews/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class ReviewForm(forms.Form):
+    username = forms.CharField(label="Azamat",max_length=100, error_messages={
+        "required":"Your name must not be empty",
+        "max_lenght": "please enter the name"
+    })
+    review_text = forms.CharField(label="Your feedback", widget=forms.Textarea, max_length=200)
+    rating  = forms.IntegerField(label="Your Rating", min_value=1,max_value=5)
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from .forms import  ReviewForm
+from .models import Review
 # Create your views here.
 def review(request):
-    return render(request, 'reviews/review.html',)
+
+    if request.method == 'POST':
+        # entered_username =  request.POST['username']
+        form =  ReviewForm(request.POST)
+        if form.is_valid():
+            review = Review(username= form.cleaned_data['username'],review_text= form.cleaned_data['review_text'],rating = form.cleaned_data['rating']) 
+            review.save()
+            
+            # print(form.cleaned_data)
+        # if entered_username == "" and len(entered_name) >= 100:
+        #     return render(request, 'reviews/review.html',{
+        #         "has_error": True,
+        #     })
+        # print(entered_username)
+            return HttpResponseRedirect("/thank-you")
+    
+    else:
+        form = ReviewForm()
+        
+    return render(request, 'reviews/review.html',{
+        "form": form
+    })
+
+
+
+def thank_you(request):
+    return render(request,"reviews/thank_you.html",{
+        "has_error": False,
+    })
